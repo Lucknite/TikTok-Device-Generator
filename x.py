c1e9a06fe3798418d9231767b7083d7ccf65eb8e
Lucas <94708257+Lucknite@users.noreply.github.com>
2024-08-26 00:20:23 +0200

    Update x.py

--- x.py
+++ x.py
@@ -1,1 +1,11 @@
+TikTok device generator (SRC)
 
+Full requests, no selenium
+
+No captcha key needed, full API
+
+Source code, can gen up to 1k/min
+
+Proxyless
+
+https://lucknite.mysellix.io https://lucknite.mysellix.io https://lucknite.mysellix.io
